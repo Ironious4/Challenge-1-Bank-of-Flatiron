5584b994e1cc217f332379c4de00d7efb89cbe29
Ironious4 <tyronekariuki@gmail.com>
2024-08-08 13:30:30 +0300

    Add modified changes

--- src/components/TransactionList.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Transaction from "./Transaction";
-function TransactionsList({items}) {
-  return(
-   <Transaction items={items}/>
-   )
-}
-
-export default TransactionsList;
